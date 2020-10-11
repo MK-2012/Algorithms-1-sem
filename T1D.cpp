95a656e4be0b94a0cb3ef3c4d90dcbf4a4e67c63
MK-2012 <koltakov.ma@phystech.edu>
2020-10-11 21:51:18 +0300

    Pull request fixes

--- T1D.cpp
+++ T1D.cpp
@@ -11,39 +11,39 @@ struct pair{
 int main(){
     deque <long long> q1;
     deque <long long> q2;
-    //std::ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    long long n = 0;
-    long long k = 0;
-    long long m = 0;
+    long long requests = 0;
+    long long queue_amount = 0;
+    long long buffer = 0;
     long long temp;
     string s;
-    cin >> n;
-    for (long long i = 0; i < n; ++i) {
+    cin >> requests;
+    for (long long i = 0; i < requests; ++i) {
         cin >> s;
         switch(s[0]) {
             case '-':
                 cout << q1.front() << '\n';
                 q1.pop_front();
-                --k;
+                --queue_amount;
                 break;
             case '+':
                 cin >> temp;
                 q2.push_back(temp);
-                ++m;
+                ++buffer;
                 break;
             case '*':
                 cin >> temp;
                 q2.push_front(temp);
-                ++m;
+                ++buffer;
                 break;
         }
-        if (k < m) {
+        if (queue_amount < buffer) {
             q1.push_back(q2.front());
             q2.pop_front();
-            --m;
-            ++k;
+            --buffer;
+            ++queue_amount;
         }
     }
     return 0;
